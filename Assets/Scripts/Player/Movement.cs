e17060430a745d016bd2aac00c0d5494d2434f00
LordFrodo <mfvelsaa@hotmail.com>
2022-01-31 22:47:18 -0500

    Avances movimiento y creacion de carpetas

---
+++ Assets/Scripts/Player/Movement.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+
+[RequireComponent(typeof(Rigidbody))]
+public class Movement : MonoBehaviour
+{
+    public static Action A_Move;
+    [Header("Variables Movimiento")]
+    [SerializeField] private float speed;
+    [Range(-1,1)]
+    [SerializeField]private float x_axis, z_axis;
+    public static float direction;
+    private Rigidbody rg;
+    // Start is called before the first frame update
+
+    // Update is called once per frame
+    private void Start()
+    {
+        rg = GetComponent<Rigidbody>();
+    }
+    void Update()
+    {
+        //x_axis = Input.GetAxis("Mouse X");
+        //z_axis = Input.GetAxis("Mouse Y");
+        if (x_axis != 0 || z_axis !=0)
+        {
+            Change_Pos(x_axis, z_axis);
+        }
+    }
+    public void Change_Pos(float x, float z)
+    {
+        Vector3 force = new Vector3(x, 0, z);
+        rg.AddForce(force*speed);
+        A_Move?.Invoke();
+    }
+}

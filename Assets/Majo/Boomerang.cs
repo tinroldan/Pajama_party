fd263d06f9375e3c19b4ba5bdffa0dc3359c9cf4
María José <maria.ramirezll@upb.edu.co>
2022-02-02 15:36:49 -0500

    2 base

--- Assets/Majo/Boomerang.cs
+++ Assets/Majo/Boomerang.cs
@@ -7,29 +7,49 @@ public class Boomerang : MonoBehaviour
     [SerializeField] float speed = 5, distance=10;
     Rigidbody rb;
     Vector3 shootPos;
+    Transform pos;
+    
 
     private void Awake() {
         rb = GetComponent<Rigidbody>();
+        pos = GetComponentInParent<Transform>();
     }
     void Start()
     {
-        GetPos();
-        rb.velocity = transform.forward * speed;
+        
+        shoot();
         
     }
 
    
     void Update() {
-        Debug.Log("magnitud" + shootPos.magnitude);
-        if (shootPos.magnitude == distance) {
-           
-            rb.velocity = -rb.velocity;
-        }
+
+        GoandReturn();
+      
+
+
     }
     void GetPos() {
        shootPos = transform.position;
     }
-   
 
-    
+    void shoot() {
+        pos.SetParent(null, true);
+        GetPos();
+        rb.AddForce(transform.forward * speed);
+
+    }
+
+    void GoandReturn() {
+        if (Vector3.Distance(shootPos, transform.position) >= distance) {
+            //rb.AddForce(-transform.forward * speed);
+            rb.velocity = -rb.velocity;
+        }
+        if (transform.position.z <= shootPos.z) {
+            rb.velocity = Vector3.zero;
+        }
+    }
+
+
+
 }

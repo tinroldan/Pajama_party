18bc15fe46f13b2ac62de3d8c1d3e262e8993d6e
María José <maria.ramirezll@upb.edu.co>
2022-02-03 16:57:35 -0500

    much better2

--- Assets/Majo/Boomerang.cs
+++ Assets/Majo/Boomerang.cs
@@ -8,13 +8,13 @@ public class Boomerang : MonoBehaviour
     Rigidbody rb;
     Vector3 shootPos,dir;
     [SerializeField]Transform originalPos, maxPos;
-    Transform pos ;
+   
     bool returnB=false, shooted=false;
     
 
     private void Awake() {
         rb = GetComponent<Rigidbody>();
-        pos = GetComponentInParent<Transform>();
+        
        // originalPos = GetComponentInParent<Transform>().position;
        
     }
@@ -33,10 +33,11 @@ public class Boomerang : MonoBehaviour
    
 
     void shoot() {
-        pos.SetParent(null, true);
+        transform.SetParent(null, true);
         shootPos=transform.position;
         rb.AddForce(transform.forward * speed);
         shooted = true;
+        returnB = false;
     }
 
     void GoandReturn() {
@@ -54,14 +55,15 @@ public class Boomerang : MonoBehaviour
         dir = originalPos.position-transform.position;
         Debug.Log(dir.magnitude);
         // transform.position += (transform.position - originalPos * -speed) * Time.deltaTime;
-        rb.MovePosition(transform.position + (dir *10* Time.deltaTime));
+        rb.MovePosition(transform.position + (dir *20* Time.deltaTime));
         if (dir.magnitude <= 0.1f) {
             Debug.Log("Hola");
             rb.velocity = Vector3.zero;
             shooted = false;
             returnB = false;
+            transform.SetParent(originalPos,true);
         }
-        //< >
+        
     }
 
 

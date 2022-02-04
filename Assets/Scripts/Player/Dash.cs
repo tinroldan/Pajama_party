f0377aae00e803cbeaea085e72836e9e6e7a2930
LordFrodo <mfvelsaa@hotmail.com>
2022-02-04 14:06:20 -0500

    Dash implementado
    Funciona el Dash dando click y tiene una funcion para cuando se haga el boton para la version Movil

--- Assets/Scripts/Player/Dash.cs
+++ Assets/Scripts/Player/Dash.cs
@@ -5,12 +5,44 @@ using UnityEngine;
 
 public class Dash : MonoBehaviour
 {
-  
+    [SerializeField] private float dash_time, dash_speed, initial_speed;
+    Rigidbody rg;
+    Transform player;
+    Movement mov;
+
+    private void Start()
+    {
+        rg = GetComponent<Rigidbody>();
+        player = GetComponent<Transform>();
+        mov = GetComponent<Movement>();
+    }
+
     // Start is called before the first frame update
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetButtonDown("Fire1"))
+        {
+            StartCoroutine(Dash_coroutine());
+        }
+        Debug.Log(rg.velocity);
+    }
+    IEnumerator Dash_coroutine()
+    {
+        float start_time = Time.time;
+        initial_speed = mov.max_speed;
+        mov.max_speed = dash_speed;
+        while (Time.time < start_time + dash_time)
+        {
+            rg.AddForce( player.forward * dash_speed);
+            yield return null;
+        }
+        mov.max_speed = initial_speed;
+
+    }
+    public void Star_Dash()
+    {
+        StartCoroutine(Dash_coroutine());
     }
 }

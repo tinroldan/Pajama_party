cf3088142bd8a48b6d8f2346c1c2ca02c79a8397
BecerraJose1 <42262295+BecerraJose1@users.noreply.github.com>
2022-03-11 00:58:03 -0500

    Sound Implementation 1
    Escena, sonidos, falta subir la modificación de movement y player2Boomerang

---
+++ Assets/managerSound.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class managerSound : MonoBehaviour
+{
+    Movement mov;
+    ManagerJoystick mj;
+    public AudioSource dieSound;
+    public AudioSource soundMovement;
+    public AudioSource soundShooting;
+  
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void soundBoton()
+    {
+        
+    }
+
+    public void soundDie ()
+    {
+        
+        if(dieSound.isPlaying == false)
+        {
+            dieSound.Play();
+        }
+    }
+
+    public void soundMove()
+    {
+        /* if(soundMovement.isPlaying == false)
+        {
+            soundMovement.Play();
+        }
+        */
+        soundMovement.Play();
+        
+    }
+
+
+    public void soundShoot()
+    {
+        soundShooting.Play();
+    }
+    
+}

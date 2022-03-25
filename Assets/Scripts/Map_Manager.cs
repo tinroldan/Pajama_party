ad0d33e73f4762e6cdc1d27607a36cfd04fe5109
LordFrodo <mfvelsaa@hotmail.com>
2022-03-25 12:32:05 -0500

    Avances sln bug

--- Assets/Scripts/Map_Manager.cs
+++ Assets/Scripts/Map_Manager.cs
@@ -16,9 +16,10 @@ public class Map_Manager : MonoBehaviour {
 
     private void Start() {
         players = GameObject.FindGameObjectsWithTag("Player");
+        change_mp = false;
     }
     private void FixedUpdate() {
-
+        Debug.Log(change_mp);
         if (Mov_Camera.local == false) {
             if (players_deaths == players.Length - 1 && counter == 0) {
                 counter = 1;
@@ -33,6 +34,7 @@ public class Map_Manager : MonoBehaviour {
             DisableCanvas();
         } else return;
 
+        
     }
 
     private void DisableCanvas() {
@@ -41,13 +43,13 @@ public class Map_Manager : MonoBehaviour {
                 players_deaths = 0;
                 counter = 0;
             }
-            score_panel.SetActive(false);
+            score_panel?.SetActive(false);
         }
     }
 
     private void ChangeMap() {
         if (Mapchanger != null) Mapchanger();
-        score_panel.SetActive(true);
+        score_panel?.SetActive(true);
         int rnd = Random.Range(0, maps.Length - 1);
         for (int i = 0; i < maps.Length; i++) {
             maps[i].SetActive(false);

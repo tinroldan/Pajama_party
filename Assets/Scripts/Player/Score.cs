305c19a885612fddfc4febf362d6d0e7203c938e
María José <maria.ramirezll@upb.edu.co>
2022-03-31 17:51:07 -0500

    Pierde puntos cuando cae

--- Assets/Scripts/Player/Score.cs
+++ Assets/Scripts/Player/Score.cs
@@ -2,8 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Score : MonoBehaviour
-{
+public class Score : MonoBehaviour {
+    public delegate void ScoreEvents();
+    public event ScoreEvents Substraction;
+    [SerializeField] AnimatorController animatorController;
    [SerializeField] Test_boomerang test_Boomerang;
     public int myScore =0;
     // Start is called before the first frame update
@@ -11,9 +13,15 @@ public class Score : MonoBehaviour
     private void Awake() {
        // test_Boomerang = GetComponentInChildren<Test_boomerang>();
         test_Boomerang.Score += AddScore;
+        animatorController.Fall += SubsScore;
     }
 
     public void AddScore() {
         myScore += 1;
+    }
+    public void SubsScore() {
+        if (myScore == 0) return;
+        myScore -= 1;
+        if (Substraction != null) Substraction();
     }
 }

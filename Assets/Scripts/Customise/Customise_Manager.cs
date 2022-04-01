d8a44b10584f34dbe7986d1179db7e55bbd1f375
tinroldan <martin.montoya@upb.edu.co>
2022-04-01 15:21:24 -0500

    customise menu online

---
+++ Assets/Scripts/Customise/Customise_Manager.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Customise_Manager : MonoBehaviour
+{
+    [SerializeField]
+    GameObject[] buttonsCharacters, buttonsPijamas, buttonsBoomerangs;
+    [SerializeField]
+    Online_skin skin;
+
+    [SerializeField]
+    Image faceImage, pijamaImage,boomerangImg;
+
+
+    [SerializeField]
+    Sprite[] faces;
+    [SerializeField]
+    Sprite[] boomerangs;
+    [SerializeField]
+    Color[] pijamas;
+
+
+    void Start()
+    {
+        faceImage.sprite = faces[skin.face];
+        boomerangImg.sprite = boomerangs[skin.boomerang];
+        pijamaImage.color = pijamas[skin.pijama];
+        UpdateButtons();
+    }
+
+    void UpdateButtons()
+    {
+        for (int i = 0; i < buttonsCharacters.Length; i++)//face
+        {
+            buttonsCharacters[i].SetActive(false);
+        }
+        for (int i = 0; i < buttonsPijamas.Length; i++)//pijama
+        {
+            buttonsPijamas[i].SetActive(false);
+        }
+        for (int i = 0; i < buttonsBoomerangs.Length; i++)//boomerang
+        {
+            buttonsBoomerangs[i].SetActive(false);
+        }
+        buttonsCharacters[skin.face].SetActive(true);
+        buttonsPijamas[skin.pijama].SetActive(true);
+        buttonsBoomerangs[skin.boomerang].SetActive(true);
+    }
+
+    public void selectFace(int face)
+    {
+        skin.face = face;
+        faceImage.sprite = faces[skin.face];
+        UpdateButtons();
+    }
+
+    public void selectPijama(int pijama)
+    {
+        skin.pijama = pijama;
+        pijamaImage.color = pijamas[skin.pijama];
+        UpdateButtons();
+    }
+
+    public void selectBoomerang(int boomerang)
+    {
+        skin.boomerang = boomerang;
+        boomerangImg.sprite = boomerangs[skin.boomerang];
+        UpdateButtons();
+    }
+
+}

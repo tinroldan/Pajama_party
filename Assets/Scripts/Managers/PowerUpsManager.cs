b3af0aadbe32f939c600b4ce96f2e2bec82f18f8
LordFrodo <mfvelsaa@hotmail.com>
2022-04-08 01:22:10 -0500

    Cambio unicacion Managers & creacion Power Up Manager
    Ya los power ups spawnean aleatoriamente en el mapa despues de 10 (se puede cambiar desde el editor). Falta implementar todos los spawns de los power ups en todos los mapas

---
+++ Assets/Scripts/Managers/PowerUpsManager.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUpsManager : MonoBehaviour
+{
+    Map_Manager map_manager;
+    [SerializeField] GameObject power_up;
+    [SerializeField] private float spawn_time;
+    private float time;
+    private int rnd;
+    // Start is called before the first frame update
+    void Start()
+    {
+        time = 0;
+        map_manager = GetComponent<Map_Manager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time += Time.deltaTime;
+        if (time >= spawn_time)
+        {
+            time = 0;
+            SpawnPowerUp();
+        }
+    }
+
+    private void SpawnPowerUp()
+    {
+        int spawns = map_manager.maps[map_manager.current_map].transform.GetChild(1).transform.childCount;
+        Transform[] spawnpoints = new Transform[spawns];
+        for (int i = 0; i < spawnpoints.Length; i++)
+        {
+            spawnpoints[i] = map_manager.maps[map_manager.current_map].transform.GetChild(1).GetChild(i).transform;
+        }
+        int past_rnd = rnd;
+        rnd =  Random.Range(0, spawns);
+        while (past_rnd == rnd) rnd = Random.Range(0, spawns);
+        Instantiate(power_up, spawnpoints[rnd]);
+        Debug.Log(rnd);
+
+    }
+}
